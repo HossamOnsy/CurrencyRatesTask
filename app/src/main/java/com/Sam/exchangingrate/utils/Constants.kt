0800b2033fb83d2012683b5c9e9fc76df314fb5a
HossamOnsy <HossamEldeenOnsy@yahoo.com>
2019-06-12 08:39:10 +0200

    config Edited

--- app/src/main/java/com/Sam/exchangingrate/utils/Constants.kt
+++ app/src/main/java/com/Sam/exchangingrate/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.Sam.exchangingrate.utils
 
-import com.Sam.exchangingrate.BuildConfig.API_KEY
+//import com.Sam.exchangingrate.BuildConfig.API_KEY
 
 // Changed it to be here Just for you to be able to test right away
 val BASE_URL = "https://revolut.duckdns.org/";
